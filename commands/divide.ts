523e41e45d2d3d797ffca5d9af4ebf2a79f00da4
stopher991 <74286514+stopher991@users.noreply.github.com>
2021-10-22 11:34:56 -0700

    New Math

---
+++ commands/divide.ts
@@ -0,0 +1,32 @@
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'Math',
+    description: 'Divide up to 10 numbers together.',
+
+    slash: false,
+    testOnly: true,
+    
+    expectedArgs: '<num1> <num2> [num3] ... [num10]',
+    minArgs: 2,
+    maxArgs: 10,
+    syntaxError: {['Incorrect Usage!']: 'Please use `{PREFIX}divide {ARGUMENTS}`'},
+
+    callback: ({ channel, args }) => {
+        // Get numbers to divide
+        const num1 = parseInt(args[0])
+        const num2 = parseInt(args[1])
+        const num3 = parseInt(args[2]) || 0
+        const num4 = parseInt(args[3]) || 0
+        const num5 = parseInt(args[4]) || 0
+        const num6 = parseInt(args[5]) || 0
+        const num7 = parseInt(args[6]) || 0
+        const num8 = parseInt(args[7]) || 0
+        const num9 = parseInt(args[8]) || 0
+        const num10 = parseInt(args[9]) || 0
+        // Find quotient of numbers
+        const quotient = num1 / num2 / num3 / num4 / num5 / num6 / num7 / num8 / num9 / num10;
+        //Reply with quotient
+        return `The quotient is ${quotient}`
+    },
+} as ICommand

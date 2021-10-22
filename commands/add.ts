6742e23535f2d3047a2232893ddfd1c236ad79c3
stopher991 <74286514+stopher991@users.noreply.github.com>
2021-10-22 10:55:39 -0700

    Minimalize code

--- commands/add.ts
+++ commands/add.ts
@@ -13,19 +13,13 @@ export default {
     syntaxError: {['Incorrect Usage!']: 'Please use `{PREFIX}add {ARGUMENTS}`'},
 
     callback: ({ channel, args }) => {
+        // Maintain 0 starting point
+        const sum = 0
         // Get numbers to add
-        const num1 = parseInt(args[0])
-        const num2 = parseInt(args[1])
-        const num3 = parseInt(args[2]) || 0
-        const num4 = parseInt(args[3]) || 0
-        const num5 = parseInt(args[4]) || 0
-        const num6 = parseInt(args[5]) || 0
-        const num7 = parseInt(args[6]) || 0
-        const num8 = parseInt(args[7]) || 0
-        const num9 = parseInt(args[8]) || 0
-        const num10 = parseInt(args[9]) || 0
-        // Find sum of numbers
-        const sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+        args.forEach(element => {
+            // Add numbers
+            sum += element
+        });
         //Reply with sum
         return `The sum is ${sum}`
     },
